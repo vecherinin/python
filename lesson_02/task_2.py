7caa25f7451e69525b34e5cc838ae0c778adaad4
Ivan <wetscherinin@gmail.com>
2021-10-27 22:40:18 +0300

    Edited

--- lesson_02/task_2.py
+++ lesson_02/task_2.py
@@ -1,30 +1,33 @@
-arr = [
+original_lists = [
     ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов'],
     ['примерно в', '23', 'часа', '8', 'минут', '03', 'секунд', 'температура', 'воздуха', 'была', '-5', 'градусов Цельсия', 'темп', 'воды', '+12', 'градусов', 'Цельсия']
 ]
+# Вставка в список отдельных строк с кавычками, до числа и после
+def add_quotes(x):
+    x.insert(i, '"')
+    x.insert(i + 2, '"')
 
-for list in arr:
-    print('\nИсходный список:\n', list)
+for each_list in original_lists:
+    print('\nИсходный список:\n', each_list)
     i = 0
-    while i < len(list):
-        if list[i].isdigit():
-            list[i] = list[i].zfill(2)
-            list.insert(i, '"')
-            list.insert(i + 2, '"')
-            i += 2
-        elif list[i][0] in "+-":
-            list[i] = list[i].zfill(3)
-            list.insert(i, '"')
-            list.insert(i + 2, '"')
-            i += 2
+    while i < len(each_list):
+        # Добавление кавычек для числительных без знаков + или -
+        if each_list[i].isdigit():
+            # Добавление нуля перед числом из одного знака
+            each_list[i] = each_list[i].zfill(2)
+            add_quotes(each_list)
+        elif each_list[i][0] in "+-":
+            each_list[i] = each_list[i].zfill(3)
+            add_quotes(each_list)
         else:
-            i += 1
-    print('\nРезультирующий список:\n', list)
+            i += -1
+        i += 2
+    print('\nРезультирующий список:\n', each_list)
 
-    string = ' '.join(list).split('"')
-
+    # Удаление пробелов между кавычками и числом (со знаком или без)
+    each_list = ' '.join(each_list).split('"')
     idx = 1
-    while idx < len(string):
-        string[idx] = string[idx].strip()
+    while idx < len(each_list):
+        each_list[idx] = each_list[idx].strip()
         idx += 2
-    print('\nСтрока:\n', '"'.join(string),'\n\n')
+    print('\nСтрока:\n', '"'.join(each_list),'\n\n')
