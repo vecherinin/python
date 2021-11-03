20f1a8af949f5fd7d46a6e9ce5ce833bb487794d
Ivan <wetscherinin@gmail.com>
2021-11-03 08:06:27 +0300

    Created

---
+++ lesson_03/task_03_5.py
@@ -0,0 +1,28 @@
+from random import choice
+
+nouns = ['автомобиль', 'лес', 'огонь', 'город', 'дом']
+adverbs = ['сегодня', 'вчера', 'завтра', 'позавчера', 'ночью', 'гораздо']
+adjectives = ['веселый', 'яркий', 'зеленый', 'утопичный', 'мягкий', 'вялый', 'ароматный']
+
+
+def get_jokes(num, n=nouns, adv=adverbs, adj=adjectives):
+    """get_jokes (num, n=nouns, adv=adverbs, adj=adjectives)
+    Choose random three words from each list
+    and return a list of strings
+    with a certain number of sentences.
+
+    Optional keyword arguments:
+    num: number (int) of sentences to output.
+    n: list of nouns (default: nouns).
+    adv: list of adverbs (default: adverbs).
+    adj: list of adjectives (default: adjectives).
+    """
+    output = []
+    for _ in range(qt):
+        """"""
+        chosen_n = choice(n)
+        chosen_adv = choice(adv)
+        chosen_adj = choice(adj)
+        output.append(f'{chosen_n} {chosen_adv} {chosen_adj}')
+    return output
+print(get_jokes.__doc__)
