3a5e20d7f51fd03f216a6b78eaf3446eceb93420
Ivan <wetscherinin@gmail.com>
2021-11-07 12:20:10 +0300

    Updated

--- lesson_04/task_04_5.py
+++ lesson_04/task_04_5.py
@@ -1,9 +1,9 @@
 from utils import currency_rates
 
 url = 'http://www.cbr.ru/scripts/XML_daily.asp'
-
 currency_input = input('Введите буквенный код валюты: ')
 output = currency_rates(url, currency_input)
+
 if output is None:
     output = 'Не найдена валюта'
 
