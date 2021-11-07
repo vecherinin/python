7473f2dc5b2adee9344637937050b3366d3b9e26
Ivan <wetscherinin@gmail.com>
2021-11-07 12:40:42 +0300

    Updated

--- lesson_04/task_04_5.py
+++ lesson_04/task_04_5.py
@@ -1,10 +1,15 @@
 from utils import currency_rates
 
 url = 'http://www.cbr.ru/scripts/XML_daily.asp'
-currency_input = input('Введите буквенный код валюты: ')
+currency_input = input('Введите трёхбуквенный код валюты: ')
 output = currency_rates(url, currency_input)
 
-if output is None:
+if len(currency_input) == 0:
+    print('Код валюты не введён.')
+elif len(currency_input) > 3 or len(currency_input) < 3:
+    print('Ошибка ввода.\nКод валюты должен состоять из трёх букв.')
+elif output is None:
     output = 'Не найдена валюта'
-
-print(currency_input.upper(), ': ', output, sep='')
+    print(currency_input.upper(), ': ', output, sep='')
+else:
+    print(currency_input.upper(), ': ', output, sep='')
