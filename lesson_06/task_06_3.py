e7b86af9f4faa88e58edb8b433582a4079479dca
Ivan <wetscherinin@gmail.com>
2021-11-15 02:58:51 +0300

    created

--- lesson_06/task_06_3.py
+++ lesson_06/task_06_3.py
@@ -5,22 +5,24 @@ hobbies = file_hobbies.read()
 list_fio = fio.replace(',', ' ').split('\n')
 list_hobbies = hobbies.split('\n')
 
+output = dict()
 i = 0
-if len(list_fio) > len(list_hobbies):
-    while True:
-        dict = {list_fio[i]:list_hobbies[i]}
-# i = 0
-# while True:
-#     if i > len(list_hobbies):
-#         output = {list_fio[i]: None}
-#     elif i > len(list_fio):
-#         exit(1)
-#     else:
-#         output = {list_fio[i]: list_hobbies[i]}
-#
-#     i += 1
+while True:
+    if i >= len(list_hobbies):
+        if i < len(list_fio):
+            output[list_fio[i]] = None
+        else:
+            break
+    elif i > len(list_fio):
+        break
+        exit(1)
+    elif i < len(list_fio) and i < len(list_hobbies):
+        output[list_fio[i]] = list_hobbies[i]
+    i += 1
 
-print(dict)
-# print(len(list_fio))
+result = open('task_3_rezult.txt', 'w+', encoding='utf-8')
+result.write(str(output))
+
 file_fio.close()
 file_hobbies.close()
+result.close()
